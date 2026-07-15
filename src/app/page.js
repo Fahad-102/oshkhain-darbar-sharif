a03662b4577b892378af1366489302961990e403
Muhammad Fahad Jamal <muhammadfahadbinjamal@gamil.com>
2026-07-16 03:09:16 +0600

    home page background

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
-"use client"
+"use client";
+
+import Image from "next/image";
 import Banner from "./component/Banner";
 import BiographyCard from "./component/BiographyCard";
 import DarbarTeachings from "./component/DarbarTeachings";
@@ -7,12 +9,25 @@ import RashidAhmedBiographyCard from "./component/RashidAhmedBiographyCard";
 
 export default function Home() {
   return (
-   <div className="max-w-7xl mx-auto">
-    <MarqueeNotice/>
-    <Banner/>
-    <DarbarTeachings/>
-    <BiographyCard/>
-    <RashidAhmedBiographyCard/>
-   </div>
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0 z-0 pointer-events-none">
+        <Image
+          src="/calligraphy-bg.jpg" 
+          alt="Calligraphy Background"
+          fill
+          className="object-cover opacity-20" 
+          priority
+          quality={75}
+        />
+      </div>
+
+      <div className="relative z-10 max-w-7xl mx-auto">
+        <MarqueeNotice />
+        <Banner />
+        <DarbarTeachings />
+        <BiographyCard />
+        <RashidAhmedBiographyCard />
+      </div>
+    </div>
   );
 }
